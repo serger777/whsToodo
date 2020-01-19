f1764db86b3517c573bc6679768c46472b959628
sergey gerasimov <sergey.gerasimov@realize-it.ru>
2020-01-19 21:45:53 +0300

    Updates

---
+++ precache-manifest.6b571d039442de5e47bf3e7d486601b9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f2c94a39a9b0218d948d8a3ecf743e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f0013dd847bce59f936",
+    "url": "/static/css/main.7782ad19.chunk.css"
+  },
+  {
+    "revision": "12d6bb47cfea60c92876",
+    "url": "/static/js/2.bbfcc589.chunk.js"
+  },
+  {
+    "revision": "92f90c9d83808ba1f3241736bda278f0",
+    "url": "/static/js/2.bbfcc589.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f0013dd847bce59f936",
+    "url": "/static/js/main.6f6c1c76.chunk.js"
+  },
+  {
+    "revision": "1ff66976dd05a87e816f",
+    "url": "/static/js/runtime-main.0e2ef1d3.js"
+  },
+  {
+    "revision": "1fccabf380cb91b99baee4ebddcf58aa",
+    "url": "/static/media/error.1fccabf3.png"
+  },
+  {
+    "revision": "0a1558bf5081fe0d139f0eb344d36182",
+    "url": "/static/media/fork.0a1558bf.svg"
+  },
+  {
+    "revision": "3feade3d4db28bbf7d006510b64309a4",
+    "url": "/static/media/github.3feade3d.svg"
+  },
+  {
+    "revision": "2ae91a719f8fce8c480a57678bf105b9",
+    "url": "/static/media/linkid.2ae91a71.svg"
+  },
+  {
+    "revision": "bfcb6328fbf9d7112b5991301c49d0fa",
+    "url": "/static/media/mail.bfcb6328.svg"
+  },
+  {
+    "revision": "60ef2abb42876cec5600e3f0c90d5b50",
+    "url": "/static/media/phone.60ef2abb.svg"
+  },
+  {
+    "revision": "a3b344a246a1de75f22227e0e72f5216",
+    "url": "/static/media/star.a3b344a2.svg"
+  },
+  {
+    "revision": "22582ca41c65310b5651bd42e31e4e02",
+    "url": "/static/media/vk.22582ca4.svg"
+  }
+]);
